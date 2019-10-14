72cb0a27fb04f02bdb49f33854cfaa4add909a6c
Bharath Simha <bharath.grandhe@utu.global>
2019-10-14 13:54:50 +0530

    First button added

--- taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/TaxFreeButton.kt
+++ taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/TaxFreeButton.kt
@@ -1,18 +1,29 @@
 package global.ututaxfree.taxfreeandroidui
 
 import android.content.Context
+import android.graphics.Color
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 /**
  * Created by Bharath Simha Gupta on 10/14/2019.
  */
 
-class TaxFreeButton {
+class TaxFreeButton(context: Context) : AppCompatButton(context) {
+
+    init {
+        onInitialize()
+    }
 
     companion object {
         @JvmStatic
         fun showToast(c: Context, message: String) {
             Toast.makeText(c, message, Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun onInitialize() {
+        setBackgroundColor(Color.parseColor("#00b398"))
+        setTextColor(Color.parseColor("#FFFFFF"))
     }
 }
