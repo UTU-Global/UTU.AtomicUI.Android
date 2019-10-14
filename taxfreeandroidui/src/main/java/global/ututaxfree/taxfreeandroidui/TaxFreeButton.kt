320b0d57c31f539eb8b97acef916f5fe92476681
Bharath Simha <bharath.grandhe@utu.global>
2019-10-14 14:13:22 +0530

    UI changes and constructor changes for button

--- taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/TaxFreeButton.kt
+++ taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/TaxFreeButton.kt
@@ -2,6 +2,7 @@ package global.ututaxfree.taxfreeandroidui
 
 import android.content.Context
 import android.graphics.Color
+import android.util.AttributeSet
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 
@@ -9,9 +10,18 @@ import androidx.appcompat.widget.AppCompatButton
  * Created by Bharath Simha Gupta on 10/14/2019.
  */
 
-class TaxFreeButton(context: Context) : AppCompatButton(context) {
+class TaxFreeButton : AppCompatButton {
 
-    init {
+    constructor(context: Context) : super(context) {
+        onInitialize()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        onInitialize()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr) {
         onInitialize()
     }
 
@@ -25,5 +35,6 @@ class TaxFreeButton(context: Context) : AppCompatButton(context) {
     private fun onInitialize() {
         setBackgroundColor(Color.parseColor("#00b398"))
         setTextColor(Color.parseColor("#FFFFFF"))
+        setPadding(50,50,50,50)
     }
 }
