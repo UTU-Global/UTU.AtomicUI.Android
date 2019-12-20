fd65f06982ec1d54f7726e60f479b82934b8f3b3
KollaLikhitha <likhitha.kolla@utu.global>
2019-12-20 14:47:37 +0530

    Completed atomic design for friends list

---
+++ app/src/main/java/global/ututaxfree/androidui/ContactsActivity.java
@@ -0,0 +1,51 @@
+package global.ututaxfree.androidui;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import global.ututaxfree.taxfreeandroidui.WaveSideBar;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    private RecyclerView rvContacts;
+    private WaveSideBar sideBar;
+
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initData();
+        initView();
+    }
+
+    private void initView() {
+        setContentView(R.layout.activity_contacts);
+        rvContacts = (RecyclerView) findViewById(R.id.rv_contacts);
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        rvContacts.setAdapter(new ContactsAdapter(contacts, R.layout.item_contacts));
+        sideBar = (WaveSideBar) findViewById(R.id.side_bar);
+        sideBar.setOnSelectIndexItemListener(new WaveSideBar.OnSelectIndexItemListener() {
+            @Override
+            public void onSelectIndexItem(String index) {
+                for (int i=0; i<contacts.size(); i++) {
+                    if (contacts.get(i).getIndex().equals(index)) {
+                        ((LinearLayoutManager) rvContacts.getLayoutManager()).scrollToPositionWithOffset(i, 0);
+                        return;
+                    }
+                }
+            }
+        });
+    }
+
+    private void initData() {
+        contacts.addAll(Contact.getEnglishContacts());
+    }
+}
