3e3eee6f5d364694a094e235a6ab1af2c4f973f5
Bharath Simha <bharath.grandhe@utu.global>
2019-12-27 17:05:07 +0530

    Atomic Dialog, HeaderView, TextInput, Loader and ViewPager added

--- app/src/main/java/global/ututaxfree/androidui/MainActivity.kt
+++ app/src/main/java/global/ututaxfree/androidui/MainActivity.kt
@@ -1,10 +1,8 @@
 package global.ututaxfree.androidui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import global.ututaxfree.taxfreeandroidui.AtomicToast
-import global.ututaxfree.taxfreeandroidui.ToastClosedListener
+import global.ututaxfree.taxfreeandroidui.AtomicDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,17 +12,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         taxFreeB.setOnClickListener {
-            AtomicToast.show(
-                context = applicationContext,
-                view = mainLayout,
-                actionText = "Testing Snackbar!!!!",
-                actionType = AtomicToast.TYPE_ERROR,
-                listener = object : ToastClosedListener {
-                    override fun onToastClosed() {
-                        Toast.makeText(applicationContext, "Toast closed", Toast.LENGTH_LONG).show()
+            /*  AtomicToast.show(
+                  context = applicationContext,
+                  view = mainLayout,
+                  actionText = "Testing Snackbar!!!!",
+                  actionType = AtomicToast.TYPE_ERROR,
+                  listener = object : ToastClosedListener {
+                      override fun onToastClosed() {
+                          Toast.makeText(applicationContext, "Toast closed", Toast.LENGTH_LONG).show()
+                      }
+                  }
+              )*/
+
+            AtomicDialog(
+                "Log out", "Are you sure you want to log out?",
+                "Log out", "Cancel",
+                object : AtomicDialog.OnDialogButtonClickListener {
+                    override fun onButtonClick(isPositive: Boolean) {
+
                     }
-                }
-            )
+
+                }, false
+            ).show(supportFragmentManager, "LogoutDialog")
         }
     }
 }
---
+++ taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/AtomicHeaderView.kt
@@ -0,0 +1,118 @@
+package global.ututaxfree.taxfreeandroidui
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.appcompat.widget.AppCompatTextView
+
+/**
+ * Created by Bharath Simha Gupta on 2019-10-27.
+ */
+class AtomicHeaderView(context: Context?, attrs: AttributeSet?) : LinearLayout(
+    context, attrs
+) {
+
+    lateinit var navigationUpButton: ImageButton
+    lateinit var menuButton: AppCompatTextView
+    lateinit var headerLabel: AppCompatTextView
+
+    companion object {
+        const val BACK = "back"
+        const val CLOSE = "close"
+
+        const val WHITE = 1
+        const val BLACK = 0
+    }
+
+    init {
+        onInit(attrs)
+    }
+
+    private fun onInit(attrs: AttributeSet?) {
+
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)
+                as LayoutInflater
+        val header = inflater.inflate(R.layout.header_view, null)
+
+        navigationUpButton = header.findViewById(R.id.headerAction)
+        menuButton = header.findViewById(R.id.headerMenu)
+        headerLabel = header.findViewById(R.id.headerText)
+
+        val a = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.AtomicHeaderView, 0, 0
+        )
+        val headerLabel =
+            a.getString(R.styleable.AtomicHeaderView_headerLabel)
+        header.findViewById<AppCompatTextView>(R.id.headerText).text = headerLabel
+
+        val isTransparent = a.getBoolean(R.styleable.AtomicHeaderView_isTransparentHeader, false)
+        if (isTransparent) {
+            header.findViewById<RelativeLayout>(R.id.coreHeaderLayout).background =
+                ColorDrawable(Color.TRANSPARENT)
+        }
+
+        val menuText = a.getString(R.styleable.AtomicHeaderView_menuText)
+        if (!TextUtils.isEmpty(menuText)) {
+            header.findViewById<AppCompatTextView>(R.id.headerMenu).text = menuText
+        }
+
+        val menuIcon = a.getResourceId(R.styleable.AtomicHeaderView_menuIcon, -1)
+        if (menuIcon != -1) {
+            header.findViewById<AppCompatTextView>(R.id.headerMenu)
+                .setCompoundDrawablesWithIntrinsicBounds(
+                    menuIcon, 0, 0, 0
+                )
+        }
+
+        when (a.getString(R.styleable.AtomicHeaderView_actionType)) {
+
+            BACK -> {
+                when (a.getInt(R.styleable.AtomicHeaderView_actionIconColor, BLACK)) {
+                    WHITE -> {
+                        header.findViewById<AppCompatTextView>(R.id.headerText)
+                            .setTextColor(Color.WHITE)
+                        navigationUpButton.setImageResource(R.drawable.ic_back_white)
+                    }
+                    BLACK -> {
+                        header.findViewById<AppCompatTextView>(R.id.headerText)
+                            .setTextColor(Color.BLACK)
+                        navigationUpButton.setImageResource(R.drawable.ic_back_black)
+                    }
+                }
+            }
+            CLOSE -> {
+                when (a.getInt(R.styleable.AtomicHeaderView_actionIconColor, BLACK)) {
+                    WHITE -> {
+                        header.findViewById<AppCompatTextView>(R.id.headerText)
+                            .setTextColor(Color.WHITE)
+                        if (isTransparent) {
+                            navigationUpButton.setImageResource(R.drawable.ic_close_scan)
+                        } else {
+                            navigationUpButton.setImageResource(R.drawable.ic_close_white)
+                        }
+                    }
+                    BLACK -> {
+                        header.findViewById<AppCompatTextView>(R.id.headerText)
+                            .setTextColor(Color.BLACK)
+                        navigationUpButton.setImageResource(R.drawable.ic_close)
+                    }
+                }
+            }
+
+        }
+        orientation = VERTICAL
+        gravity = Gravity.CENTER_VERTICAL
+
+        addView(header)
+
+        a.recycle()
+    }
+}
