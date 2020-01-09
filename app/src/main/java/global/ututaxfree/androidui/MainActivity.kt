f7bed9fee8b339c0f6e2fccaa1327c22b2b83790
Bharath <gvnbsg@gmail.com>
2020-01-09 15:46:31 +0530

    Toast implementation changed

--- app/src/main/java/global/ututaxfree/androidui/MainActivity.kt
+++ app/src/main/java/global/ututaxfree/androidui/MainActivity.kt
@@ -1,7 +1,6 @@
 package global.ututaxfree.androidui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import global.ututaxfree.taxfreeandroidui.AtomicToast
 import global.ututaxfree.taxfreeandroidui.ToastClosedListener
@@ -9,32 +8,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var toast: AtomicToast? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         taxFreeB.setOnClickListener {
-            AtomicToast.show(
+            toast = AtomicToast(
                 context = applicationContext,
                 view = mainLayout,
-                actionText = "Testing Snackbar!!!!",
+                actionText = "Testing Snackbar!!!! asdyfgoas a uasdifuapsudhf oash opfiasdpo iasoi foaisdj oasij askdgf alshfasbdf asd",
                 actionType = AtomicToast.TYPE_ERROR,
                 listener = object : ToastClosedListener {
                     override fun onToastClosed() {
-                        Toast.makeText(applicationContext, "Toast closed", Toast.LENGTH_LONG).show()
                     }
                 })
+            toast!!.show()
+        }
+    }
 
-            AtomicToast.show(
-                context = applicationContext,
-                view = mainLayout,
-                actionText = "Testing Snackbar!!!!",
-                actionType = AtomicToast.TYPE_WARNING,
-                listener = object : ToastClosedListener {
-                    override fun onToastClosed() {
-                        Toast.makeText(applicationContext, "Toast closed", Toast.LENGTH_LONG).show()
-                    }
-                })
+    override fun onBackPressed() {
+        if (toast != null) {
+            toast!!.dismiss()
         }
     }
 }
--- taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/AtomicToast.kt
+++ taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/AtomicToast.kt
@@ -16,121 +16,119 @@ import com.androidadvance.topsnackbar.TSnackbar
  * Created by Bharath Simha Gupta on 10/25/2019.
  */
 
-class AtomicToast {
+class AtomicToast(
+    private var context: Context,
+    private var view: View,
+    private var actionText: String,
+    private var actionType: String,
+    private var listener: ToastClosedListener?
+) {
+
+    private var isEllipsized = false
+    private var snackBar: TSnackbar? = null
+
+    init {
+        onBuildToast()
+    }
+
+    private fun onBuildToast() {
+        val handler = Handler()
+        snackBar = TSnackbar.make(view, "", TSnackbar.LENGTH_INDEFINITE)
+        val customView =
+            (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                .inflate(R.layout.utu_toast, null)
+        val snackBarView = snackBar!!.view as TSnackbar.SnackbarLayout
+        val parentParams = snackBarView.layoutParams as FrameLayout.LayoutParams
+        parentParams.width = FrameLayout.LayoutParams.MATCH_PARENT
+        parentParams.height = FrameLayout.LayoutParams.WRAP_CONTENT
+        parentParams.gravity = Gravity.TOP
+        snackBarView.layoutParams = parentParams
+
+        val textView = customView.findViewById<AppCompatTextView>(R.id.toastText)
+        textView.text = actionText
+
+        textView.setOnClickListener {
+            if (isEllipsized) {
+                handler.removeCallbacksAndMessages(null)
+                textView.maxLines = 4
+            }
+        }
+
+        val closeToast = customView.findViewById<ImageButton>(R.id.closeToast)
+
+        when (actionType) {
+            TYPE_SUCCESS -> {
+                textView.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_success, 0, 0, 0
+                )
+                closeToast.setImageResource(R.drawable.ic_success_close)
+            }
+            TYPE_WARNING -> {
+                textView.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_warning, 0, 0, 0
+                )
+                closeToast.setImageResource(R.drawable.ic_warning_close)
+            }
+            TYPE_ERROR -> {
+                textView.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_error, 0, 0, 0
+                )
+                closeToast.setImageResource(R.drawable.ic_error_close)
+            }
+        }
+
+        closeToast.setOnClickListener {
+            handler.removeCallbacksAndMessages(null)
+            if (snackBar != null) {
+                snackBar!!.dismiss()
+            }
+        }
+
+        snackBar!!.setCallback(object : TSnackbar.Callback() {
+            override fun onDismissed(snackbar: TSnackbar?, event: Int) {
+                super.onDismissed(snackbar, event)
+                handler.removeCallbacksAndMessages(null)
+                listener?.onToastClosed()
+            }
+        })
+
+        snackBarView.setPadding(0, 0, 0, 0)
+        snackBarView.addView(customView, 0)
+
+        val vto: ViewTreeObserver = textView.viewTreeObserver
+        vto.addOnGlobalLayoutListener {
+            val layout: Layout = textView.layout
+            val lines = layout.lineCount
+            if (lines > 0) {
+                if (layout.getEllipsisCount
+                        (lines - 1) > 0
+                ) {
+                    isEllipsized = true
+                }
+            }
+        }
+        handler.postDelayed({
+            if (snackBar != null) {
+                snackBar!!.dismiss()
+            }
+        }, 2500)
+    }
+
+    fun show() {
+        if (snackBar != null) {
+            snackBar!!.show()
+        }
+    }
+
+    fun dismiss() {
+        if (snackBar != null) {
+            snackBar!!.dismiss()
+        }
+    }
 
     companion object {
-
         const val TYPE_SUCCESS = "small"
         const val TYPE_WARNING = "regular"
         const val TYPE_ERROR = "big"
-
-        private var isEllipsized = false
-
-        @JvmStatic
-        fun show(
-            context: Context, view: View, actionText: String, actionType: String,
-            listener: ToastClosedListener?
-        ) {
-            show(context, view, actionText, actionType, listener, true)
-        }
-
-        @JvmStatic
-        fun show(
-            context: Context, view: View, actionText: String, actionType: String,
-            listener: ToastClosedListener?, isCloseButton: Boolean
-        ) {
-            onBuildToast(context, view, actionText, actionType, listener, isCloseButton)
-        }
-
-        private fun onBuildToast(
-            context: Context, view: View, actionText: String, actionType: String,
-            listener: ToastClosedListener?, isCloseButton: Boolean
-        ) {
-
-            val handler = Handler()
-            val snackBar: TSnackbar = TSnackbar.make(view, "", TSnackbar.LENGTH_INDEFINITE)
-            val customView =
-                (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
-                    .inflate(R.layout.utu_toast, null)
-            val snackBarView = snackBar.view as TSnackbar.SnackbarLayout
-            val parentParams = snackBarView.layoutParams as FrameLayout.LayoutParams
-            parentParams.width = FrameLayout.LayoutParams.MATCH_PARENT
-            parentParams.height = FrameLayout.LayoutParams.WRAP_CONTENT
-            parentParams.gravity = Gravity.TOP
-            snackBarView.layoutParams = parentParams
-
-            val textView = customView.findViewById<AppCompatTextView>(R.id.toastText)
-            textView.text = actionText
-
-            textView.setOnClickListener {
-                if (isEllipsized) {
-                    handler.removeCallbacksAndMessages(null)
-                    textView.maxLines = 4
-                }
-            }
-
-            val closeToast = customView.findViewById<ImageButton>(R.id.closeToast)
-            if (isCloseButton) {
-                closeToast.visibility = View.VISIBLE
-            }
-
-            when (actionType) {
-                TYPE_SUCCESS -> {
-                    textView.setCompoundDrawablesWithIntrinsicBounds(
-                        R.drawable.ic_success, 0, 0, 0
-                    )
-                    if (isCloseButton) {
-                        closeToast.setImageResource(R.drawable.ic_success_close)
-                    }
-                }
-                TYPE_WARNING -> {
-                    textView.setCompoundDrawablesWithIntrinsicBounds(
-                        R.drawable.ic_warning, 0, 0, 0
-                    )
-                    if (isCloseButton) {
-                        closeToast.setImageResource(R.drawable.ic_warning_close)
-                    }
-                }
-                TYPE_ERROR -> {
-                    textView.setCompoundDrawablesWithIntrinsicBounds(
-                        R.drawable.ic_error, 0, 0, 0
-                    )
-                    if (isCloseButton) {
-                        closeToast.setImageResource(R.drawable.ic_error_close)
-                    }
-                }
-            }
-
-            closeToast.setOnClickListener {
-                handler.removeCallbacksAndMessages(null)
-                snackBar.dismiss()
-            }
-
-            snackBar.setCallback(object : TSnackbar.Callback() {
-                override fun onDismissed(snackbar: TSnackbar?, event: Int) {
-                    super.onDismissed(snackbar, event)
-                    handler.removeCallbacksAndMessages(null)
-                    listener?.onToastClosed()
-                }
-            })
-
-            snackBarView.setPadding(0, 0, 0, 0)
-            snackBarView.addView(customView, 0)
-
-            val vto: ViewTreeObserver = textView.viewTreeObserver
-            vto.addOnGlobalLayoutListener {
-                val layout: Layout = textView.layout
-                val lines = layout.lineCount
-                if (lines > 0) {
-                    if (layout.getEllipsisCount
-                            (lines - 1) > 0
-                    ) {
-                        isEllipsized = true
-                    }
-                }
-            }
-            handler.postDelayed({ snackBar.dismiss() }, 2500)
-            snackBar.show()
-        }
     }
 }
