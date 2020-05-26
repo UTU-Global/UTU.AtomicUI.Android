3cdb109a83bd3798acca7d6bb8de9bfa4b555b35
Bharath <bharath.grandhe@utu.global>
2020-05-26 12:45:02 +0530

    Atomic input changes

--- taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/AtomicTextInput.kt
+++ taxfreeandroidui/src/main/java/global/ututaxfree/taxfreeandroidui/AtomicTextInput.kt
@@ -23,7 +23,7 @@ import com.google.android.material.textfield.TextInputEditText
 class AtomicTextInput @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.editTextStyle
+    defStyleAttr: Int = R.attr.textInputStyle
 ) :
     TextInputEditText(context, attrs, defStyleAttr), TextWatcher {
 
